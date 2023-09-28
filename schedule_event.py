bbbbec6d8d65d799ac2c8112e95a8d9f0c09fa53
Christine Spang <christine@spang.cc>
2023-09-28 14:55:25 -0700

    remove comments intended for copilot

--- schedule_event.py
+++ schedule_event.py
@@ -11,16 +11,13 @@ NYLAS_API_KEY = os.environ.get("NYLAS_API_KEY")
 if not NYLAS_API_KEY:
     raise Exception("Please set the NYLAS_API_KEY environment variable")
 
-# parse up to 10 email addresses using click
+
 @click.command()
 @click.option("--email", "-e", multiple=True, help="Email address of guest")
 @click.option("--title", "-t", help="Title of event")
 @click.option("--description", "-d", help="Description of event")
-# parse start date using click
 @click.option("--start", "-s", help="Start date of event")
-# end date too
 @click.option("--end", help="End date of event")
-# click parse option to pass in grant ID
 @click.option("--grant-id", "-g", help="Grant ID")
 def schedule_event(email, title, description, start, end, grant_id):
     """Schedule an event with Nylas"""
